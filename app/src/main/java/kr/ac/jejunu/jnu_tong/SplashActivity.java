c1d24ae5314b7f943eb422955b3cd6f9bbec5c11
seung-yeol <oks153123@naver.com>
2018-04-24 03:58:48 +0900

    아이콘, 스플레쉬 적용

---
+++ app/src/main/java/kr/ac/jejunu/jnu_tong/SplashActivity.java
@@ -0,0 +1,27 @@
+package kr.ac.jejunu.jnu_tong;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.ac.jejunu.jnu_tong.main.MainActivity;
+
+/**
+ * Created by seung-yeol on 2018. 4. 24..
+ */
+
+public class SplashActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        new Handler().postDelayed(() -> {
+            startActivity(intent);
+            finish();
+        }, 300);
+    }
+}
